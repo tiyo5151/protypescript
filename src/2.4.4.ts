c251a861440008474e39329ff257afa2310700fc
tiyo5151 <s1F102400224@iniad.org>
2024-06-11 15:33:06 +0900

    README.txt change

--- src/2.4.4.ts
+++ src/2.4.4.ts
@@ -1,13 +1,37 @@
-const left1 = -5, right1 = 0;
-console.log(left1 < right1); // true と表示される
+// const left1 = -5, right1 = 0;
+// console.log(left1 < right1); // true と表示される
 
-const left2 = 100n, right2 = 50n;
-console.log(left2 >= right2); // true と表示される
+// const left2 = 100n, right2 = 50n;
+// console.log(left2 >= right2); // true と表示される
 
-const left3 = -10, right3 = 0;
-console.log(left3 > right3); // false と表示される
+// const left3 = -10, right3 = 0;
+// console.log(left3 > right3); // false と表示される
 
-const left4 = 12n, right4 = 8n;
-console.log(left4 <= right4); // false と表示されるs
+// const left4 = 12n, right4 = 8n;
+// console.log(left4 <= right4); // false と表示されるs
 
-console.log("apple" < "orange");
+// console.log("apple" < "orange");
+
+import { createInterface } from "readline";
+
+const rl = createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question(`パスワードを入力してください：` ,(password) => {
+    if (password==='hogehoge') {
+        console.log("ようこそ！");
+    } else {
+        console.log('誰？');
+    }
+    rl.close()
+})
+
+const str: any = "3";
+
+// true が表示される（文字列が数値に変換されるので）
+console.log(str == 3); 
+
+// false が表示される（異なる型である文字列と数値を比較しているので）
+console.log(str === 3);
---
+++ src/2.4.5.ts
@@ -0,0 +1,30 @@
+// const t = true, f = false;
+
+// console.log(t && t); // true と表示される
+// console.log(t && f); // false と表示される
+// console.log(f && f); // false と表示される
+
+// console.log(t || t); // true と表示される
+// console.log(t || f); // true と表示される
+// console.log(f || f); // false と表示される
+
+// import { createInterface } from "readline";
+
+// const rl = createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// });
+
+// rl.question(`数値を入力してください：`, (line) => {
+//     const num = Number(line);
+//     if (0<=num&&num<100) {
+//         console.log(`${num}は0以上100未満です`)
+//     } else {
+//         console.log(`${num}は0以上100未満ではありません`)
+//     }
+//     if (!Number.isNaN(num)) {
+//         console.log(num,"はNaNではありません")
+//     }
+//     rl.close()
+// })
+

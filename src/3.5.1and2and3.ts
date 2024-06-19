614743d64a071ac476996acb8ce91ba35f507885
tiyo5151 <s1F102400224@iniad.org>
2024-06-19 12:06:33 +0900

    gakubuti

---
+++ src/3.5.1and2and3.ts
@@ -0,0 +1,38 @@
+// const arr = [0,123,-456*100];
+// console.log(arr);
+// const arr2 = [100,"文字列",false];
+// console.log(arr2);
+// const arr3 = [4,5,6];
+// const arr4 = [1,2,3,...arr3];
+// console.log(arr4);
+// const arr1 = [0,123,-456*100];
+// console.log(arr1[0]);
+// console.log(arr1[1]);v fccc
+// arr[1] = 5400;
+// console.log(arr);
+
+// const arr:number[] = [1,10,100];
+// const arr2:string[] = [123,-456];
+
+// const arr1:boolean[] = [false,true];
+// const arr2:Array<{name:string}> = [
+//     {name:"山田さん"},{name:"田中さん"},{name:"鈴木さん"}
+// ];
+// console.log(arr2[0])
+
+// const arr = [1,10,100];
+// const arr1 :(string|number|boolean)[] = [100,"文字列",false]
+
+// const arr:readonly number[] = [1,10,100];
+// arr[1] = -500;
+
+// const arr = [1,10,100];
+// arr.push(1000);
+// console.log(arr);
+// arr.push("foobar")
+// const arr:readonly number[] = [1,10,100];
+// arr.push(1000);
+
+const arr = [1,10,100];
+console.log(arr.includes(100));
+console.log(arr.includes(50));

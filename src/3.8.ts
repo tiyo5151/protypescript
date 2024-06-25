38cd24097348888d8fc1f1eba397a25e0130dfe9
tiyo5151 <s1F102400224@iniad.org>
2024-06-25 16:30:47 +0900

    function

--- src/3.8.ts
+++ src/3.8.ts
@@ -2,7 +2,7 @@ type User = {
     name:string;
     age:number;
     premiumUser:boolean;
-}
+};
 
 const data:string= `
 uhyo,26,1
@@ -10,9 +10,16 @@ John Smith,17,0
 Mary Sue,14,1
 `;
 
-const users:User[] = {
-    
+const users:User[] = []
+
+const separates = data.split("\n");
+
+for (const separate of separates) {
+    const S = separate.split(",")
+    users.push({name:S[0]})
 }
+
+
 
 for (const user of users) {
     if (user.premiumUser) {
---
+++ src/4.1.ts
@@ -0,0 +1,79 @@
+// console.log(range(5,10));
+
+// function range(min:number,max:number):number[] {
+//     const result = [];
+//     for (let i = min;i<=max;i++) {
+//         result.push(i);
+//     }
+//     return result
+// }
+
+// console.log(range(5,10));
+
+// function helloWorldTimes(n:number):void {
+//     for (let i = 0;i<n;i++) {
+//         console.log("helloworld");
+//     }
+// }
+
+// helloWorldTimes(10)
+
+// function helloWorldTimes2(n:number):void {
+//     if (n>=100) {
+//         console.log(`${n}回なんて無理です！！！`);
+//         return;
+//     }
+//     for(let i = 0;i<n;i++) {
+//         console.log("Hello,world!")
+//     }
+// }
+
+// helloWorldTimes2(5);
+// helloWorldTimes2(150);
+
+// function toSeconds(hours:number,minutes:number,seconds:number):number {
+//     return hours*3600+minutes*60+seconds
+// }
+
+// console.log(toSeconds(4,50,30))
+
+// function toSeconds(hours:number,minutes:number,seconds:number):number {
+//     return hours*3600
+//         + minutes*60
+//         +seconds;
+// }
+
+// type Human = {
+//     height:number;
+//     weight:number;
+// };
+// const calcBMI = function(human:Human):number {
+//     return human.weight/human.height**2;
+// };
+// const uhyo:Human = {height:1.84,weight:72};
+
+// console.log(calcBMI(uhyo));
+
+// type Human = {
+//     height:number;
+//     weight:number;
+// };
+// const calcBMI = function({height,weight}:Human):number{
+//     return weight / height**2;
+// };
+// const uhyo:Human = {height:1.84,weight:72};
+// console.log(calcBMI(uhyo));
+
+// const uhyo: Human = { height: 1.84, weight: 72 };
+// // エラー: Block-scoped variable 'calcBMI' used before its declaration.
+// console.log(calcBMI(uhyo));
+
+// type Human = {
+//   height: number;
+//   weight: number;
+// };
+// const calcBMI = function({
+//   height, weight
+// }: Human): number {
+//   return weight / height ** 2;
+// };

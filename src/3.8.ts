5b0f33bf9f3902450c3f0ad37723a5dccddfa8b7
tiyo5151 <s1F102400224@iniad.org>
2024-06-26 02:36:12 +0900

    arrow

--- src/3.8.ts
+++ src/3.8.ts
@@ -1,30 +1,30 @@
-type User = {
-    name:string;
-    age:number;
-    premiumUser:boolean;
-};
+// type User = {
+//     name:string;
+//     age:number;
+//     premiumUser:boolean;
+// };
 
-const data:string= `
-uhyo,26,1
-John Smith,17,0
-Mary Sue,14,1
-`;
+// const data:string= `
+// uhyo,26,1
+// John Smith,17,0
+// Mary Sue,14,1
+// `;
 
-const users:User[] = []
+// const users:User[] = []
 
-const separates = data.split("\n");
+// const separates = data.split("\n");
 
-for (const separate of separates) {
-    const S = separate.split(",")
-    users.push({name:S[0]})
-}
+// for (const separate of separates) {
+//     const S = separate.split(",")
+//     users.push({name:S[0]})
+// }
 
 
 
-for (const user of users) {
-    if (user.premiumUser) {
-        console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
-    } else {
-        console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`)
-    }
-}
+// for (const user of users) {
+//     if (user.premiumUser) {
+//         console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
+//     } else {
+//         console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`)
+//     }
+// }
--- src/4.1.ts
+++ src/4.1.ts
@@ -77,3 +77,42 @@
 // }: Human): number {
 //   return weight / height ** 2;
 // };
+
+// type Human = {
+//     height:number;
+//     weight:number;
+// };
+
+// const calcBMI = ({
+//     height,weight
+// }:Human):number => {
+//     return weight/height**2;
+// };
+// const calcBMI = ({
+//     height,weight
+// }:Human):number => weight/height**2;
+
+// const uhyo:Human = {height:1.84,weight:72};
+// console.log(calcBMI(uhyo));
+
+type Human = {
+    height:number;
+    weight:number;
+};
+type ReturnObj = {
+    bmi:number
+}
+
+const calcBMIObject = ({
+    height,weight
+}:Human):ReturnObj => ({
+    bmi: weight /height**2
+});
+
+// const calcBMIObject2 = ({
+//     height, weight
+//   }: Human): ReturnObj => {
+//     bmi: weight / height ** 2;
+//   };
+
+

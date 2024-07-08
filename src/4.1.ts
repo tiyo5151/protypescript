426c88aecc7266d4225fc3838f25c17945e1a9b2
tiyo5151 <s1F102400224@iniad.org>
2024-07-08 23:57:52 +0900

    7/8

--- src/4.1.ts
+++ src/4.1.ts
@@ -159,14 +159,14 @@
 // console.log(sum(123,456));
 // console.log(sum());
 
-const sum = (...args:number[]):number => {
-    let result = 0;
-    for (const num of args) {
-        result += num;
-    }
-    return result;
-};
+// const sum = (...args:number[]):number => {
+//     let result = 0;
+//     for (const num of args) {
+//         result += num;
+//     }
+//     return result;
+// };
 
-const nums = [1,2,3,4,5];
-console.log(sum(...nums));
-console.log(sum(...nums,6,...nums));
+// const nums = [1,2,3,4,5];
+// console.log(sum(...nums));
+// console.log(sum(...nums,6,...nums));
